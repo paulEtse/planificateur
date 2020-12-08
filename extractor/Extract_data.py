a0e5c92c873a1b12caca5713d0ae7e857eca2776
RoussPat <patrickrouss@gmail.com>
2020-12-08 16:48:41 +0100

    chemins relatifs

--- extractor/Extract_data.py
+++ extractor/Extract_data.py
@@ -7,13 +7,15 @@ livraisons_xlsx = pd.read_excel("./data/livraison guides.xlsx",parse_dates=['liv
 
 
 
-path = "./data/Sequencement EST Modified.xlsx"
+pathOUEST = "./data/Sequencement OUEST Modified.xlsx"
+pathEST = "./data/Sequencement EST Modified.xlsx"
 
 
 def extract_tasks_from_excel(path):
     xlsx = pd.read_excel(path,sheet_name=None)
-    time = pd.DataFrame(index = list(xlsx.keys()),columns=["Tps_pieds","Tps_guides","Tps_total"])
-    requirements = pd.DataFrame(index = list(xlsx.keys()),columns=["Livraison","Stock","Max_livraion"])
+    time = pd.DataFrame(index = list(xlsx.keys()),columns=["Tps_pieds","Tps_guides","Tps_total","Tps_QC"])
+    req_mat = pd.DataFrame(index = list(xlsx.keys()),columns=["Livraison","Stock","Max_livraion"])
+    req_task = pd.DataFrame(index = list(xlsx.keys()),columns=["tasks_req"])
     for task in xlsx : 
         for index,value in enumerate(xlsx[task].iloc[:,8]):
             if value == "Tps pieds":
@@ -22,14 +24,16 @@ def extract_tasks_from_excel(path):
                 guides = xlsx[task].iloc[index+1,8]
             if value == "Tps total":
                 total = xlsx[task].iloc[index+1,8]
-                
+            if value == "Tps QC":
+                QC = xlsx[task].iloc[index+1,8]
         time.loc[task,"Tps_pieds"] = pieds
         time.loc[task,"Tps_guides"] = guides
         time.loc[task,"Tps_total"] = total
-        requirements = np.asarray(xlsx[task].iloc[:,0].dropna())
+        time.loc[task,"Tps_QC"] = QC
+        req_mat = np.asarray(xlsx[task].iloc[:,0].dropna())
         livraison = []
         stock = []
-        for ref in requirements:
+        for ref in req_mat:
             if ref[0] =='W':
                 livraison.append(ref)
             else:
@@ -41,8 +45,15 @@ def extract_tasks_from_excel(path):
                 if ref in livraison:
                     if date > max_livraion:
                         max_livraion = date
-        requirements.loc[task,"Max_livraion"] = max_livraion      
-        requirements.loc[task,"Livraison"] = list(livraison)
-        requirements.loc[task,"Stock"] = list(stock)
-    return(time,requirements)
-extract_tasks_from_excel(path)
+        req_mat.loc[task,"Max_livraion"] = max_livraion      
+        req_mat.loc[task,"Livraison"] = list(livraison)
+        req_mat.loc[task,"Stock"] = list(stock)
+
+        i = 1
+        req = []
+        while(xlsx[task.iloc[i,1]] != None):
+            req.append(xlsx[task.iloc[i,1]])
+        req_task.loc[task,"tasks_req"] = req
+
+
+return(time,req_mat,req_task)
