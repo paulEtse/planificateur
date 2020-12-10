ea14a76bd9b436fb355844599ab865828d0db8ba
RoussPat <patrickrouss@gmail.com>
2020-12-10 10:47:57 +0100

    ajout de la prise en chages des taches necessaires pour executer la tache courante dans Extract_data.py

--- main_test.py
+++ main_test.py
@@ -2,4 +2,4 @@
 from extractor import Extract_data
 
 timeOUEST, req_matOUEST, req_taskOUEST = Extract_data.extract_tasks_from_excel(Extract_data.pathOUEST)
-print(req_matOUEST)
+print(timeOUEST, req_matOUEST, req_taskOUEST )
