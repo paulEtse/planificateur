396f2b6f3653c06bf7500cda776631c14ac2f0fb
RoussPat <patrickrouss@gmail.com>
2021-01-11 09:30:44 +0100

    fiex and tested dates once and for all

--- main_test.py
+++ main_test.py
@@ -1,5 +1,21 @@
 from extractor import Extract_data
 from src.solver import SolverPLNE
+from src import date_converter
+from datetime import datetime, timedelta
 
-sol = SolverPLNE.solve_from_skratch()
-print(sol)
+date = datetime(2019,11,1,1,0,0)
+
+
+for i in range(50):
+    ts = datetime.timestamp(date)
+    print("Original date : ", date, ts)
+    worktime = date_converter.convert_to_work_time(ts)
+    print("worktime :" , worktime)
+    converted = date_converter.convert_to_timestamp(worktime)
+    print("converted date : ", datetime.fromtimestamp(converted) ,converted )
+    date = date + timedelta(days = 1)
+
+
+
+
+
