53e27d87f7ecb023b4769c6f6309a75a1a8c8dbb
Samuel Lager <samuel.lager.43@gmail.com>
2021-01-20 18:05:30 +0100

    Fix bug with script EC2

--- main_aws.py
+++ main_aws.py
@@ -4,13 +4,12 @@ import requests
 import os
 import time
 
-import pandas as pd
 from src import Solution
 from src.solver.SolveurPPC import SolveurPPC
 
 from docplex.cp.config import context
-# context.solver.agent = 'local'
-# context.solver.local.execfile = '/opt/ibm/ILOG/CPLEX_Studio201/cpoptimizer/bin/x86-64_linux/cpoptimizer'
+context.solver.agent = 'local'
+context.solver.local.execfile = '/opt/ibm/ILOG/CPLEX_Studio201/cpoptimizer/bin/x86-64_linux/cpoptimizer'
 
 shutdown = False
 if shutdown:
@@ -27,14 +26,12 @@ while changes:
     if r.status_code >= 400:
         break
 
-    startAt = datetime.now()
+    startAt = datetime.utcnow()
     ppc = SolveurPPC()
-    #print(r.json())
     starting_solution = Solution.generate_Solution_from_json(r.json())
-    solution = ppc.add_constraint(starting_solution, 10*data['DURATION']) #TO CHANGE
-    print(solution)
+    solution = ppc.add_constraint(starting_solution, 60*data['DURATION'])
 
-    #r = requests.post(baseUrl + '/project/solution', json=solution.to_json())
+    requests.put(baseUrl + '/project/solution', data=solution.to_json(), headers={"Content-Type": "application/json"})
 
     r = requests.get(baseUrl + '/project/parameters')
     data = r.json()
